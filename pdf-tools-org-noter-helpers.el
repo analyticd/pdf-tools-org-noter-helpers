648914da6d2b173d6f7e5ad33abfeee338be7741
Foobar <4829djaskdfj@gmail.com>
2018-05-18 16:02:29 -0700

    Initial commit

---
+++ pdf-tools-org-noter-helpers.el
@@ -0,0 +1,48 @@
+;; Author: @analyticd
+;; License MIT
+;; Copyright (c) 2018 @analyticd
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
+
+;; The above copyright notice and this permission notice shall be included in all
+;; copies or substantial portions of the Software.
+
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
+
+;;;###autoload
+(defun pdf-noter-create-notes-file-for-current-book ()
+  "Create the org-noter file based on the PDF file name for the current buffer."
+  (interactive)
+  (if (eq major-mode 'pdf-view-mode)    ; TODO This should be made to work with nov.el for epub books too.
+      (progn
+        (call-interactively 'bookmark-set)
+        (save-excursion
+          (let* ((book-filename buffer-file-name)
+                 (filename-non-directory (file-name-nondirectory book-filename))
+                 (filename (first (split-string filename-non-directory "\\\.")))
+                 (noter-filepath (concatenate 'string org-directory "/" filename "-notes" ".org")))
+            (if (not (file-exists-p noter-filepath))
+                (progn
+                  (with-temp-buffer (find-file noter-filepath)
+                                    (delay-mode-hooks
+                                      (point-min)
+                                      (org-insert-heading)
+                                      (insert (format " %s\n" filename))
+                                      (org-set-property "NOTER_DOCUMENT" book-filename)
+                                      (save-buffer)
+                                      (org-noter)))
+                  (call-interactively 'bookmark-jump))
+              (error "org-noter document for this book already exists; not creating one.")))))
+    (error "Must be in \"PDFView\" mode to use this function.")))
+
+(provide 'pdf-tools-org-noter-helpers)
